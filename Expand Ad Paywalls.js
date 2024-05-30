665041a52c428ead2016b104dd574d8e411957cf
Ryan Green | GreeniusGenius <74023299+GreeniusGenius@users.noreply.github.com>
2024-05-30 21:08:23 +0100

    Fixed spacing on metadata so that the name can be read.

--- Expand Ad Paywalls.js
+++ Expand Ad Paywalls.js
@@ -1,22 +1,22 @@
 // ==UserScript ==
-// @name Expand Adblocked URL
-// @namespace https://github.com/GreeniusGenius
-// @author Ryan Green
-// @version 1.0.0
-// @description Redirects shortened advert and tracking URLs via adbypass.org to the expanded URL form, useful when DNS adblockers like PiHole and browser based blockers like Ublock Origin block URL shorteners. This version is a lot less complicated than the original author's version.
-// @match *://*linkvertise.com/*
-// @match *://*linkvertise.net/*
-// @match *://*link-to.net/*
-// @exclude *://publisher.linkvertise.com/*
-// @exclude *://linkvertise.com/search*
-// @exclude *://linkvertise.com/login*
-// @exclude *://linkvertise.com/profile*
-// @exclude *://blog.linkvertise.com
-// @exclude *://blog.linkvertise.com/*
-// @exclude *://linkvertise.com/assets/vendor/*
-// @exclude *://publisher.linkvertise.com/*
-// @exclude *://link-mutation.linkvertise.com/*
-// @exclude *://linkvertise.com/assets/external/thinksuggest
+// @name         Expand Adblocked URL
+// @namespace    https://github.com/GreeniusGenius
+// @author       Ryan Green
+// @version      1.0.0
+// @description  Redirects shortened advert and tracking URLs via adbypass.org to the expanded URL form, useful when DNS adblockers like PiHole and browser based blockers like Ublock Origin block URL shorteners. This version is a lot less complicated than the original author's version.
+// @match        *://*linkvertise.com/*
+// @match        *://*linkvertise.net/*
+// @match        *://*link-to.net/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://linkvertise.com/search*
+// @exclude      *://linkvertise.com/login*
+// @exclude      *://linkvertise.com/profile*
+// @exclude      *://blog.linkvertise.com
+// @exclude      *://blog.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/vendor/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://link-mutation.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/external/thinksuggest*
 // @icon https://www.google.com/s2/favicons?sz=64&domain=adbypass.org
 // @grant CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/) - Based upon Bypass.city Team's Linkvertise Bypass Script
 // @license
--- Expand URL.js
+++ Expand URL.js
@@ -1,13 +1,13 @@
 // ==UserScript ==
-// @name Expand Shortened URL
-// @namespace https://github.com/GreeniusGenius
-// @author Ryan Green
-// @version 1.0.0
-// @description Redirects shortened tracking URLs via Expandurl.net to the expanded URL form, useful when DNS adblockers like PiHole and browser based blockers like Ublock Origin block URL shorteners.
-// @match https://go.flx1.com/click?id=*
-// @match https://tidd.ly/*
-// @match https://bit.ly/*
-// @icon https://www.google.com/s2/favicons?sz=64&domain=expandurl.net
+// @name         Expand Shortened URL
+// @namespace    https://github.com/GreeniusGenius
+// @author       Ryan Green
+// @version      1.0.0
+// @description  Redirects shortened tracking URLs via Expandurl.net to the expanded URL form, useful when DNS adblockers like PiHole and browser based blockers like Ublock Origin block URL shorteners.
+// @match        https://go.flx1.com/click?id=*
+// @match        https://tidd.ly/*
+// @match        https://bit.ly/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=expandurl.net
 // @grant none
 // ==/UserScript==
 
