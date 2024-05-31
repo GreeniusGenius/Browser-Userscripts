9869dd50d38b5761f17d313c5bbd82985234e86e
Ryan Green | GreeniusGenius <74023299+GreeniusGenius@users.noreply.github.com>
2024-05-31 13:36:02 +0100

    More suitable favicon.

--- src/Remove Other News Paywalls.user.js
+++ src/Remove Other News Paywalls.user.js
@@ -2,12 +2,12 @@
 // @name         Redirect New Civil Engineer to 12ft.io
 // @namespace    https://github.com/GreeniusGenius
 // @downloadURL  https://github.com/GreeniusGenius/Browser-Userscripts/raw/master/src/Remove%20Other%20News%20Paywalls.user.js
-// @version      1.0.0
+// @version      1.0.1
 // @description  Redirects Motorsport to 12ft.io so there is no paywall!!
 // @author       Mokshsinghji via https://gist.github.com/mokshsinghji/fea83081c00c1ed8693faed33bf2c531
 // @author       Modified by Ryan Green
 // @match        https://www.newcivilengineer.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=newcivilengineer.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=12ft.io
 // @grant        none
 // ==/UserScript==
 
