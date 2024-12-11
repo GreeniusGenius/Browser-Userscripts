8ff2135d935b22fd38a50fb57c452a029b28d4db
Ryan Green | GreeniusGenius <74023299+GreeniusGenius@users.noreply.github.com>
2024-12-11 21:52:33 +0000

    Fixed @downloadURl in metadata to actually work by fixing incorrect case

--- src/Userscripts.org to Userscripts-MIRROR.org.user.js
+++ src/Userscripts.org to Userscripts-MIRROR.org.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Userscripts.org to Userscripts-MIRROR.org
 // @namespace   us.greeni.UserscriptsToMirror
-// @downloadURL https://github.com/GreeniusGenius/Browser-Userscripts/raw/master/src/Userscripts.org%20to%20Userscripts-Mirror.org.user.js
+// @downloadURL https://github.com/GreeniusGenius/Browser-Userscripts/raw/master/src/Userscripts.org%20to%20Userscripts-MIRROR.org.user.js
 // @author      n-scripts via https://userscripts-mirror.org/scripts/review/487275, Ryan Green
 // @version     2.0
 // @description The purpose of the simple rewrite of the script, whilst using the existing regex is to improve performance and improve security. The script I based this upon, runs on all web pages, and refactors any links out to userscripts.org to the mirror site. This script takes the different approach of refactoring the URL once on userscripts.org, which means it only runs when it needs to.
