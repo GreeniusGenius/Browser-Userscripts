27d65954b078538a73da196a8e7f189f5c61991b
Ryan Green | GreeniusGenius <74023299+GreeniusGenius@users.noreply.github.com>
2024-12-11 21:45:36 +0000

    Reordered and tidied up script metadata to make it easier to interpret.

--- src/Userscripts.org to Userscripts-MIRROR.org.user.js
+++ src/Userscripts.org to Userscripts-MIRROR.org.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
+// @name        Userscripts.org to Userscripts-MIRROR.org
+// @namespace   us.greeni.UserscriptsToMirror
+// @downloadURL https://github.com/GreeniusGenius/Browser-Userscripts/raw/master/src/Userscripts.org%20to%20Userscripts-Mirror.org.user.js
+// @author      n-scripts via https://userscripts-mirror.org/scripts/review/487275, Ryan Green
+// @version     2.0
+// @description The purpose of the simple rewrite of the script, whilst using the existing regex is to improve performance and improve security. The script I based this upon, runs on all web pages, and refactors any links out to userscripts.org to the mirror site. This script takes the different approach of refactoring the URL once on userscripts.org, which means it only runs when it needs to.
+// @grant       none
 // @match       https://userscripts.org/*
 // @match       http://userscripts.org/*
-// @name        Userscripts.org to Userscripts-MIRROR.org
-// @downloadURL https://github.com/GreeniusGenius/Browser-Userscripts/raw/master/src/Userscripts.org%20to%20Userscripts-Mirror.org.user.js
 // @exclude     https://userscripts-mirror.org/*
 // @exclude     http://userscripts-mirror.org/*
-// @namespace   us.greeni.UserscriptsToMirror
-// @author      n-scripts via https://userscripts-mirror.org/scripts/review/487275, greeniusgenius
-// @version     2.0
-// @grant       none
-// @description The purpose of the simple rewrite of the script, whilst using the existing regex is to improve performance and improve security. The script I based this upon, runs on all web pages, and refactors any links out to userscripts.org to the mirror site. This script takes the different approach of refactoring the URL once on userscripts.org, which means it only runs when it needs to.
 // ==/UserScript==
 /*
 The purpose of the simple rewrite of the script, whilst using the existing regex is to improve performance and improve security.
